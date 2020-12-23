b6d91d089ea8d8d891e9695570c83e8e1f73dabd
Kazi Rabiul Islam Porag <kazi.03may@gmail.com>
2020-12-23 22:20:30 +0600

    first commit

---
+++ construct.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Student{
+
+    protected $name;
+    protected $age;
+
+    public function getName(){
+        return $this->name; //
+    }
+
+    public function getAge(){
+        return $this->age;
+    }
+
+    private function serName(){
+        $this->name = $name;
+    }
+
+    private function serAge(){
+        $this->name = $age;
+    }
+
+    public function __construct($name, $age){
+        $this->setName($name);
+        $this->setAge($age);
+    }
+
+}
+
+$ob = new student('Porag', 32);
+
+echo $ob->getName();
+echo $ob->getAge();
